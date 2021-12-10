882dc2541a72ec75fe2b286f9cc21d0cdd195451
田轩宇 <2661756211@qq.com>
2021-12-10 23:57:21 +0800

    2021年12月10日初学c++

---
+++ Fristcode.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	printf("ÄãºÃ£¬ÐùÓîÍ¬Ñ§\n");
+//	printf("Hello,Hello!");
+//	return 0;
+//}
+//int main() 
+//{
+//	//printf("%d\n", 100);
+//	printf("%u\n", sizeof(char));
+//	printf("%u\n", sizeof(short));
+//	printf("%u\n", sizeof(int));
+//	printf("%u\n", sizeof(long));
+//	printf("%u\n", sizeof(long long));
+//	printf("%u\n", sizeof(float));
+//	printf("%u\n", sizeof(double));
+//	return 0;
+//}
+//int main()
+//{
+//	int prince = 55;
+//	float weight = 80.55f;
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 19;
+//	char xing = 
+//}
+//int b = 1000;
+//int main()
+//{
+//	int a = 100;
+//	printf("a = %d\n", a);
+//	printf("b = %d\n", b);
+//	return 0;
+//
+//}
+//int a = 1000;
+//int main()
+//{
+//	//int a = 100;
+//	printf("a = %d\n", a);
+//	return 0;
+//}
+//int main() {
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main() {
+//	{
+//		int a = 100;
+//		printf("%d\n", a);
+//	}
+//	/*printf("%d\n", a);*/
+//	return 0;
+//}
+int a = 100;
+void test() {
+	printf("%d", a);
+}
+int main() {
+	printf("%d", a);
+	return 0;
+
+}
