3ba89b422410bd92062ae056838ef74095d6f61d
田轩宇 <2661756211@qq.com>
2021-12-11 23:17:15 +0800

    2021.12.11.1

---
+++ smallplane.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+//描述
+//KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机。
+//输入描述：
+//无
+//输出描述：
+//	 **
+//	 **
+//************
+//************
+//     **
+//     **
+#include<stdio.h> 
+int main() {
+	printf("     **\n");
+	printf("     **\n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *\n");
+	printf("    *  *\n");
+}
