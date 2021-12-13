6b2602a588f12f3af2902790f65cd8614648328f
田轩宇 <2661756211@qq.com>
2021-12-13 20:23:27 +0800

    2021年12月13日代码

---
+++ code20211213/code20211213/first.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <string.h>
+//int main() {
+//	printf("%d", '\060');
+//	return 0;
+//
+//}
+//int main()
+//{
+//    char arr[] = { 'b', 'i', 't' };
+//    printf("%d\n", strlen(arr));
+//    return 0;
+//}
+//
+//int compare(int a, int b) {
+//	if (a > b) {
+//		return a;
+//	}
+//	else {
+//		return b;
+//	}
+//}
+//int main(){
+//	int a, b;
+//	scanf("%d", &a);
+//	scanf("%d", &b);
+//	int bignum = compare(a,b);
+//	printf("%d", bignum);
+//}#include <stdio.h>
+//int main() {
+//	int iq;
+//	scanf("%d", &iq);
+//	if (iq >= 140) {
+//		printf("Genius");
+//	}
+//}
+//#include<stdio.h>
+//
+//int main() {
+//
+//    printf("Name    Age    Gender\n");
+//
+//    printf("---------------------\n");
+//
+//    printf("Jack    18     man\n");
+//
+//
+//#include<stdio.h>
+//int main() {
+//    int a = 40;
+//    int c = 212;
+//    int result = (-8 + 22) * a - 10 + c / 2;
+//    printf("%d", result);
+//    return 0;
+//}
