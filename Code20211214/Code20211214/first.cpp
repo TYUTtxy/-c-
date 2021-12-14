4e6abf2cbe6cd20720f3420c483b4de7e9960b18
田轩宇 <2661756211@qq.com>
2021-12-14 23:48:04 +0800

    2021年12月14日代码

---
+++ Code20211214/Code20211214/first.cpp
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main() {
+//	printf("abc\0def");
+//	return 0;
+//}
+//int main() {
+//	int a = 10;
+//	int b = -10;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main() {
+//	int a = 10;
+//	printf("%d\n", sizeof(a));
+//	return 0;
+//}
+//int main() {
+//	printf("%d\n", sizeof(int));
+//	return 0;
+//}
+//int main() {
+//	int a = 10;
+//	int b = a + 1;
+//	printf("%d\n%d\n", a, b);
+//	return 0;
+//}
+//int main() {
+//	int a = 3;
+//	int b = 5;
+//	int m = 0;
+//	/*if (a > b) {
+//		m = a;
+//	}
+//	else
+//	{
+//		m = b;
+//	}*/
+//	m = ((a > b) ? (a) : (b));
+//	printf("%d\n", m);
+//	return 0;
+// 38
+//}
+int main() {
+	int a = 3;
+	int b = 20;
+	int c = 0;
+	int d = (a -= 1,b += 2);
+	printf("%d", d);
+	return 0;
+}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//
+//}
+//int main() {
+//	return 0;
+//}
+//#include <stdio.h>
+//int main() {
+//	int iq;
+//	scanf("%d", &iq);
+//	if (iq >= 140) {
+//		printf("Genius");
+//	}
+//}
