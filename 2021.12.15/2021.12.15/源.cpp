2359e70f378eed55ed8316fe1bd159ee1bc27915
tianxuanyu <2661756211@qq.com>
2022-03-15 15:03:40 +0800

    2021.12.15

---
+++ 2021.12.15/2021.12.15/源.cpp
@@ -0,0 +1,136 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main() {
+//	int age = 19;
+//	scanf("%d", &age);
+//	if (age >= 19)
+//		printf("成年");
+//	return 0;
+//}
+//int main() {
+//	if (age < 18) {
+//		printf("青少年");
+//	}
+//	else if{
+//		printf("青年\n");
+//	}
+//	return 0;
+//}
+//int main() {
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n % 2 == 1) {
+//		printf("奇数");
+//	}
+//	return 0;
+//}
+//int main() {
+//	for (int i = 0; i <= 100; i++) {
+//		if (i % 2 == 1) {
+//			printf("%d\n",i);
+//		}
+//	}
+//	return 0;
+//}
+//int main() {
+//	int ia, ib, ic;
+//	int tem = 0;
+//	scanf("%d %d %d", &ia, &ib, &ic);
+//	if (ia <= ib)
+//	{
+//		tem = ia;
+//		ia = ib;
+//		ib = tem;
+//	}
+//	if (ib <= ic)
+//	{
+//		tem = ib;
+//		ib = ic;
+//		ic = tem;
+//	}
+//	if (ia <= ib)
+//	{
+//		tem = ia;
+//		ia = ib;
+//		ib = tem;
+//	}
+//	printf("%d %d %d", ia, ib, ic);
+//	return 0;
+//}
+//int main() {
+//	for (int i = 1; i < 100; i++) 
+//	{
+//		if (i % 3 == 0) 
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+int main() {
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+
+	return 0;
+}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+//int main() {
+//	return 0;
+//}
+
