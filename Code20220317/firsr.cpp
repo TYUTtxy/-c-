3f4d57228dfcc1fa774dd9a50c2c9e9b04355d7a
tianxuanyu <2661756211@qq.com>
2022-03-18 12:03:46 +0800

    Code20220317

---
+++ Code20220317/firsr.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void left_move(char arr[], int k) {
+//	int len = strlen(arr);
+//	for (int j = 0; j < k % len; j++)
+//	{
+//		char tmp = arr[0];
+//		for (int i = 0; i < len - 1; i++)
+//		{
+//			arr[i] = arr[i + 1];
+//		}
+//		arr[len - 1] = tmp;
+//	}
+//}
+//int main()
+//{
+//	char arr[20] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	lest_move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
---
+++ Code20220317/first.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int Sum = 0;
+//	for (int i = 10000; i <= 99999; i++)
+//	{
+//		int sum = 0;
+//		for (int j = 0; j < 4; j++)
+//		{
+//			int m = i % (int)pow(10,j);
+//			int n = i / (int)pow(10, j);
+//			sum += m * n;
+//
+//		}
+//		if (Sum == i)
+//		{
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int ch = getchar();
+//	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A', && ch <= 'z'))
+//	{
+//		printf("YES\n");
+//	}
+//	else{
+//		printf("No\n");
+//	}
+//	return 0;
+//}
+//#include<string.h>
+//void left_move(char arr[], int k) {
+//	int len = strlen(arr);
+//	for (int j = 0; j < k%len; j++)
+//	{
+//		char tmp = arr[0];
+//		for (int i = 0; i < len - 1; i++)
+//		{
+//			arr[i] = arr[i + 1];
+//		}
+//		arr[len - 1] = tmp;
+//	}
+//}
+//int main()
+//{
+//	char arr[20] = "abcdef";
+//	int k = 0;
+//	scanf("%d", &k);
+//	lest_move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+int mian()
+{
+	int ar[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+
+}
