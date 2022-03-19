88e395114e8bfd0a5724e90d607280e4e21207d8
tianxuanyu <2661756211@qq.com>
2022-03-19 13:46:52 +0800

    Code20220318

---
+++ Code20220318/first.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//void find_k(int arr[3][3], int k, int r, int c)
+//{
+//	int x = 0;
+//	int y = c - 1;
+//	if (arr[x][y] < k)
+//	{
+//		x++;
+//	}
+//	else if (arr[x][y] > y)
+//	{
+//		y--;
+//	}
+//	else 
+//	{
+//		printf("")
+//	}
+//}
+//int main()
+//{
+//
+//	int ar[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+//	int k = 7;
+//	fide_k(arr, k, 3, 3);
+//	retunr 0;
+//}
+int mian()
+{
+	return 0;
+}
