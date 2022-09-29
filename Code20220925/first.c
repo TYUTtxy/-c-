ea2987de2e90c839a8c5b508bce1cbf8d1235663
tianxuanyu <2661756211@qq.com>
2022-09-29 15:57:43 +0800

    Code20220925.exe

---
+++ Code20220925/first.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+int main(){
+//{
+//	/*char str1[30] = { "People"};
+//	char str2[] = { "china" };
+//	strcat(str1, str2);
+//	printf("%s", str1);*/
+//	char str1[10], str2[] = "china";
+//	strcpy(str1, str2);
+/*	char a[] = "AAA";
+	printf("%s",_strlwr(a))*/;
+	char a[] = "aaa";
+	printf("%s", _strupr(a));
+	return 0;
+}
